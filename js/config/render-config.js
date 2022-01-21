221303915ee4f3a2e5f6b38f40dd97f87fbd56d1
Mobbbb <491450147@qq.com>
2022-01-21 16:13:26 +0800

    base config

---
+++ js/config/render-config.js
@@ -0,0 +1,43 @@
+const RENDER_CONFIG = {
+    title: {
+        domId: 'headerWrap',
+        name: 'Visual',
+    },
+    banner: {
+        domId: 'bannerWrap',
+        renderBasic: false,
+        render: () => {},
+    },
+    card1: {
+        domId: 'card-1',
+        name: 'Card1',
+    },
+    card2: {
+        domId: 'card-2',
+        name: 'Card2',
+    },
+    card3: {
+        domId: 'card-3',
+        name: 'Card3',
+    },
+    card4: {
+        domId: 'card-4',
+        name: 'Card4',
+    },
+    card5: {
+        domId: 'card-5',
+        name: 'Card5',
+    },
+    card6: {
+        domId: 'card-6',
+        name: 'Card6',
+    },
+    card7: {
+        domId: 'card-7',
+        name: 'Card7',
+    },
+    card8: {
+        domId: 'card-8',
+        name: 'Card8',
+    },
+}
