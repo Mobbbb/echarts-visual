2e6e611f2186e2cb450052230b0f6a035e4cbf00
Mobbbb <mobbbb.top@gmail.com>
2022-01-28 15:59:26 +0800

    示例数据添加

--- js/render/render-content.js
+++ js/render/render-content.js
@@ -16,17 +16,15 @@ const renderTitle = (renderBasic, config) => {
  */
 const renderPieRoundLabel = (classNameParams, config) => {
     const { domId, data, startAngle = 0 } = config
-    const widthRate = 1
+    const labelCircleSemidiameter = 10, borderDiameterRate = 0.85
     const boxContentWidth = document.getElementById(domId).getElementsByClassName('chart-wrap')[0].clientWidth
     const boxContentHeight = document.getElementById(domId).getElementsByClassName('chart-wrap')[0].clientHeight
-    // 图表容器宽度
-    const pieContentWidth = boxContentWidth * widthRate
-    // 获取图表容器最大内切半径
-    const minRadius = pieContentWidth > boxContentHeight ? boxContentHeight : pieContentWidth
+    // 获取图表容器最大内切直径
+    const maxDiameter = boxContentWidth > boxContentHeight ? boxContentHeight : boxContentWidth
     // 文本标签引导线直径
-    const borderWidth = minRadius * 0.85
+    const borderDiameter = maxDiameter * borderDiameterRate
     // 引导线圆心坐标
-    const circleCenterPonint = [pieContentWidth / 2, boxContentHeight / 2]
+    const circleCenterPonint = [boxContentWidth / 2, boxContentHeight / 2]
 
     let totalNum = 0, labelDom = '', maxValue = 0
     data.forEach(item => {
@@ -43,14 +41,14 @@ const renderPieRoundLabel = (classNameParams, config) => {
             let Theta = 360 * (sumAngle + reverseItem.value / 2) / totalNum + startAngle
             if (Theta > 360) Theta = Theta - 360
             // 转化为直角坐标
-            let x = Math.floor(borderWidth / 2 * Math.cos((Theta / 180) * Math.PI))
-            let y = Math.floor(borderWidth / 2 * Math.sin((Theta / 180) * Math.PI))
+            let x = Math.floor(borderDiameter / 2 * Math.cos((Theta / 180) * Math.PI))
+            let y = Math.floor(borderDiameter / 2 * Math.sin((Theta / 180) * Math.PI))
             // 坐标原点平移
             x = Math.floor(x + circleCenterPonint[0])
             y = Math.floor(Math.abs(y - circleCenterPonint[1]))
             // 自身中心点平移
-            x = Math.floor(x - 10)
-            y = Math.floor(y - 10)
+            x = Math.floor(x - labelCircleSemidiameter)
+            y = Math.floor(y - labelCircleSemidiameter)
 
             let currentPercent = Math.round(reverseItem.value * 100 / totalNum)
             if (index < config.data.length - 1) {
@@ -72,11 +70,89 @@ const renderPieRoundLabel = (classNameParams, config) => {
 
     document.getElementsByClassName(classNameParams.beforeDomClass)[0].innerHTML = `
         <div class="rose-pie-label-line" 
-            style="width: ${borderWidth}px;
-            height: ${borderWidth}px;
-            left: ${circleCenterPonint[0] - borderWidth / 2}px;
-            top: ${circleCenterPonint[1] - borderWidth / 2}px;">
+            style="width: ${borderDiameter}px;
+            height: ${borderDiameter}px;
+            left: ${circleCenterPonint[0] - borderDiameter / 2}px;
+            top: ${circleCenterPonint[1] - borderDiameter / 2}px;">
         </div>
         ${labelDom}
     `
+}
+
+const registerMap = () => {
+    echarts.registerMap('ZJ-MAP', zhejiang)
+}
+
+const createCanvas = (config, size) => {
+    const labelName = '示例数据：'
+    const canvasFontSize = `${genVH(8)}px`
+    const canvasTextSize = getTextSize(labelName, canvasFontSize)
+    const { width: canvasTextWidth, height: canvasTextHeight } = canvasTextSize
+    const { width, height } = size // 画布大小
+
+    const { labelConfig = {}, data = [] } = config
+    let canvasValueWidth = 0
+    data.forEach(item => {
+        let itemWidth = getTextSize(item, canvasFontSize).width
+        canvasValueWidth = itemWidth > canvasValueWidth ? itemWidth : canvasValueWidth
+    })
+
+    const { position, firstPointOffsetX, firstPointOffsetY, secondPointLength } = labelConfig // 引导线配置
+
+    let firstPoint = [] // 折线拐点坐标
+    let secondPoint = [] // 横线末端坐标
+    let thirdPoint = [] // 矩形左上角顶点坐标
+    const textGap = genVMin(4) // 文本上线间距
+    const rectPadding = [genVMin(4), genVMin(8), genVMin(8), genVMin(8)] // 文本容器的padding
+
+    const rectWidth = canvasTextWidth + canvasValueWidth + rectPadding[1] + rectPadding[3] // 矩形宽度
+    const rectHeight = canvasTextHeight * data.length + textGap * (data.length - 1) + rectPadding[0] + rectPadding[2] // 矩形高度
+
+    if (position === 'left-top') {
+        firstPoint = [width / 2 - firstPointOffsetX, height / 2 - firstPointOffsetY]
+        secondPoint = [firstPoint[0] - secondPointLength, firstPoint[1]]
+        thirdPoint = [width / 2 - rectWidth - firstPointOffsetX - secondPointLength, firstPoint[1] - rectHeight / 2, rectWidth, rectHeight]
+    } else if (position === 'left-bottom') {
+        firstPoint = [width / 2 - firstPointOffsetX, height / 2 + firstPointOffsetY]
+        secondPoint = [firstPoint[0] - secondPointLength, firstPoint[1]]
+        thirdPoint = [width / 2 - rectWidth - firstPointOffsetX - secondPointLength, firstPoint[1] - rectHeight / 2, rectWidth, rectHeight]
+    } else if (position === 'right-top') {
+        firstPoint = [width / 2 + firstPointOffsetX, height / 2 - firstPointOffsetY]
+        secondPoint = [firstPoint[0] + secondPointLength, firstPoint[1]]
+        thirdPoint = [width / 2 + firstPointOffsetX + secondPointLength, firstPoint[1] - rectHeight / 2, rectWidth, rectHeight]
+    } else { // right-bototm
+        firstPoint = [width / 2 + firstPointOffsetX, height / 2 + firstPointOffsetY]
+        secondPoint = [firstPoint[0] + secondPointLength, firstPoint[1]]
+        thirdPoint = [width / 2 + firstPointOffsetX + secondPointLength, firstPoint[1] - rectHeight / 2, rectWidth, rectHeight]
+    }
+
+    const canvas = createHiDPICanvas(width, height, 3)
+    const ctx = canvas.getContext('2d')
+    ctx.moveTo(width / 2, height / 2)
+    ctx.lineTo(firstPoint[0], firstPoint[1])
+    ctx.lineTo(secondPoint[0], secondPoint[1])
+
+    // 矩形绘制
+    ctx.strokeStyle = '#08d7e1'
+    ctx.fillStyle = 'rgba(12, 46, 59, 0.7)'
+    ctx.lineWidth = 1
+    ctx.strokeRect(thirdPoint[0], thirdPoint[1], thirdPoint[2], thirdPoint[3])
+    ctx.fillRect(thirdPoint[0], thirdPoint[1], thirdPoint[2], thirdPoint[3])
+    ctx.stroke()
+    
+    // 文字内容绘制
+    data.forEach((item, index) => {
+        const textY = thirdPoint[1] + canvasTextHeight * (index + 1) + rectPadding[0] + textGap * index
+        ctx.beginPath()
+        ctx.font = `${canvasFontSize} Arial, "Microsoft Yahei"`
+        ctx.fillStyle = '#fdfdfd'
+        ctx.fillText(labelName, thirdPoint[0] + rectPadding[3], textY)
+        ctx.stroke()
+        ctx.beginPath()
+        ctx.fillStyle = '#07cdf5'
+        ctx.fillText(item, thirdPoint[0] + rectPadding[3] + canvasTextWidth, textY)
+        ctx.stroke()
+    })
+
+    return canvas
 }
